842c80d74136849f1cddbaf3c5cfb9665d339745
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-21 15:38:02 +0700

    Update soal-01.c

--- soal-01.c
+++ soal-01.c
@@ -23,7 +23,7 @@ void SegitigaRec(int arr[] , int n) {
 	SegitigaRec(temp, n - 1);
 	for (int i = 0; i < n ; i++) {
 	  if(i == n - 1)
-		 printf("%d ",arr[i]);
+		 printf("%d",arr[i]);
 	  else
 		 printf("%d, ",arr[i]);
 	}
